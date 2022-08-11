7e243e7ca42b9749fc2c71bc08fb3e7f12ce9464
felix-rojas <A01201946@tec.mx>
2022-08-11 12:53:23 -0500

    add anon f(x)

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
-// var, const, console (log, info , warn, error)
+// functions
+function check_answer() {
+    const numero_pulpo_perfecto = 1;
+    let answer = window.prompt("¿Cuántos pulpos perfectos existen?");
+    if (answer == numero_pulpo_perfecto) {
+        alert("RESPUESTA CORRECTA");
+    } else {
+        alert("SOLAMENTE EXISTE UN PULPO PERFECTO");
+    }
+}
 
+// funciones anónimas
+() => console.log("anonymous");
+
+// funciones anónimas asignada a variables
+const anon = () => console.log("anonymous");
+
+
+// var, const, console (log, info , warn, error)
 // variable local (block-scoped local)
 let pulpo = "Paul";
 
@@ -7,23 +24,27 @@ let pulpo = "Paul";
 var pulpo_cantor = "Timothy";
 
 // variable (const)
-const pulpo_perfecto = "Perfec-pulpo es inmutable y único, por tanto: perfecto"
-const numero_pulpo_perfecto = 1;
+const pulpo_perfecto = "Perfec-pulpo es inmutable y único, por tanto: perfecto";
+
 
 // console function to print value assigned to prototype
 console.log(pulpo);
 console.log(pulpo_cantor);
 
-for (let i = 0; i < 10; i++){
-    console.log(pulpo_cantor + " canta " + i);
+for (let i = 0; i < 10; i++) {
+  console.log(pulpo_cantor + " canta " + i);
 }
 
 // console.log(i);
+// alerts, prompts, confirms
 
-console.log("¿Cuántos pulpos perfectos hay?");
-console.log(numero_pulpo_perfecto);
+window.alert("ACCESANDO A PULPOS.HTML");
+check_answer();
 console.log(pulpo_perfecto);
 console.info("Perfec-Pulpo es inmutable");
 console.warn("QUITA LOS LOGS");
+console.assert(2 === 2);
+
 console.error("I am actually not an error");
-console.assert(1 === 2);
+
+anon();
