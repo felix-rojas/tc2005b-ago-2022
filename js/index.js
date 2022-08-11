922a677745df4cf97e48e3fd0aeede8eff689bb1
felix-rojas <A01201946@tec.mx>
2022-08-11 11:58:43 -0500

    add js script to index

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// var, const, console (log, info , warn, error)
+
+// variable local
+let pulpo = "Paul";
+
+// variable global
+var pulpo_cantor = "Timothy";
+
+// console function to print value assigned to prototype
+console.log(pulpo);
+console.log(pulpo_cantor);
+
