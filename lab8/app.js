f40efbdcca479bb1d60cd1555dda19c3374e1a60
felix-rojas <A01201946@tec.mx>
2022-08-18 12:46:21 -0500

    create server, no response

--- lab8/app.js
+++ lab8/app.js
@@ -1,6 +1,33 @@
-const filesystem = require('fs'); // handle files and paths
+const filesystem = require("fs"); // handle files and paths
+const http = require('http');
+const server = http.createServer( (request, response) => {
+    console.log(http.request.url);
+});
 
-// 
-filesystem.writeFileSync('./media/hello.txt', 'Im writing this on node!');
+// cannot create directory, use the function to make directory
+filesystem.writeFileSync("./media/hello.txt", "Im writing this on node!");
 
 console.log("Hello im in node");
+
+const arreglo = [222, 123, 444, 5000];
+
+// sort array with timer (awful lmao, dont)
+for (let item of arreglo) {
+  setTimeout(() => {
+    console.log(item);
+  }, item);
+}
+
+function average(arreglo_a_promediar) {
+  let sum = 0;
+  let count = 0;
+  for (let item of arreglo) {
+    sum += item;
+    count += 1;
+  }
+  return sum/count;
+}
+
+console.log("El promedio es: "+average(arreglo));
+
+server.listen(3100);
