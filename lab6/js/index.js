6ac38a982e98a429dd3e643b0b8953946db2b18f
felix-rojas <A01201946@tec.mx>
2022-08-19 17:12:00 -0500

    add password verification and validation

--- lab6/js/index.js
+++ lab6/js/index.js
@@ -1,6 +1,51 @@
+const pass_verify = document.getElementById("password_verify");
+const pass = document.getElementById("password");
+const user = document.getElementById("username");
+const login_button = document.getElementById("login");
+const nav_login_button = document.getElementById("login_nav");
 
-let pass = document.getElementById("password");
-let user = document.getElementById("username");
+let usuario = "";
+let contra = "";
+let verify = "";
 
-console.log(user.onkeyup.value);
-console.log(pass.onkeyup.value);
+console.log(pass_verify.value)
+console.log(pass.value)
+console.log(user.value)
+
+
+
+user.onkeyup = () => {
+    usuario = user.value;
+    console.log(usuario);
+}
+
+pass.onkeyup = () => {
+    contra = pass.value;
+    console.log(contra);
+}
+
+pass_verify.onkeyup = () => {
+    verify = pass_verify.value;
+    console.log(verify);
+}
+
+
+login_button.onclick = () => {
+    if (verify !== contra) {
+        window.alert("Password missmatch")
+    } else {
+        window.alert("Succesful account creation")
+        login_button.innerText = "logout"
+        nav_login_button.innerText = "logout"
+    }
+}
+
+nav_login_button.onclick = () => {
+    if (verify !== contra) {
+        window.alert("Password missmatch")
+    } else {
+        window.alert("Succesful account creation")
+        login_button.innerText = "logout"
+        nav_login_button.innerText = "logout"
+    }
+}
