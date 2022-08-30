aa84683430e6c8239ca4c707ea968be69db9d71e
felix-rojas <A01201946@tec.mx>
2022-08-30 16:18:46 -0500

    add media

--- lab14/controllers/bird.controller.js
+++ lab14/controllers/bird.controller.js
@@ -1,36 +1,21 @@
 const path = require('path');
-const Ganador = require('../models/bird.model');
+const Birds = require('../models/bird.model');
 
 exports.getBird = (req, res, next) => {
     
     console.log(req.cookies);
 
-    const numero_clicks = req.cookies.numero_clicks ? req.cookies.numero_clicks : 0;
-
-    const ultimo_ganador = req.session.ultimo_ganador ? req.session.ultimo_ganador : false;
-
-    res.render(path.join('paul', 'trivia.ejs'), {
-        clicks: req.cookies.numero_clicks ? req.cookies.numero_clicks : numero_clicks,
-        ultimo_ganador: ultimo_ganador,
+    res.render(path.join('bird', 'birds.ejs'), {
     });
 };
 
 exports.postBird = (req, res, next) => {
 
-    let nombreGanador = '';
-    if (Math.floor(Math.random() * 2) == 0) {
-        nombreGanador = req.body.visitante;
-    } else {
-        nombreGanador = req.body.local;
-    }
+    const aBird = new Birds(bird_URL);
+    aBird.save();
 
-    const unGanador = new Ganador(nombreGanador);
-    unGanador.save();
+    req.session.ultimo_Birds = aBird;
 
-    req.session.ultimo_ganador = unGanador;
-
-    const numero_clicks = req.cookies.numero_clicks ? Number(req.cookies.numero_clicks) + 1 : 1;
-    
     res.cookie("numero_clicks" , numero_clicks, {
         httpOnly: true,
     });
@@ -38,9 +23,9 @@ exports.postBird = (req, res, next) => {
     console.log(req.cookies);
     console.log(req.body);
 
-    res.render(path.join('paul', 'ganador.ejs'), {
-        ganador: unGanador.nombre, 
-        ganadores: Ganador.fetchAll(),
+    res.render(path.join('bird', 'birds.ejs'), {
+        Birds: aBird.nombre, 
+        yourBirds: Birds.fetchAll(),
         clicks: numero_clicks,
     });
 };
