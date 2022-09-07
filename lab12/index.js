6aba1a8c41d33f7f84df01014bd9f649dcb93217
felix-rojas <a01201946@tec.mx>
2022-09-07 13:00:33 -0500

    use ejs lab12

--- lab12/index.js
+++ lab12/index.js
@@ -18,18 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Middleware general response to all routes
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Middleware
-// This middleware is what allows us to crete proper responses to requests
-app.use((request, response, next) => {
-    console.log("Middleware!");
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-  });
-  
-  // every time we send a response, you must tell it to go to the next middleware
-  app.use((request, response, next) => {
-    console.log("Otro middleware!");
-    next();
-  });
 
 const rutas_garbo = require('./routes/garbo.routes');
 app.use('/sample', rutas_garbo);
@@ -38,7 +26,7 @@ const rutas_pulpitos= require('./routes/pulpitos.routes');
 app.use('/pulpos', rutas_pulpitos);
 
 app.use("/hola", (request, response, next) => {
-  response.send('Hola desde la ruta "/hola". Utiliza la ruta /sample para ver un ejemplo, o haz click'+'<br><br><a href= "/sample"> Regresa a sample </a>');
+  response.render(path.join('includes','return.ejs'));
 });
 
 // To add routes:
@@ -52,4 +40,4 @@ app.use((request, response, next) => {
     response.send('La ruta no existe :( '+'<br><br><a href= "/sample"> Regresa a sample </a>');
   });
 
-app.listen(3000);
+app.listen(3100);
--- lab12/routes/pulpitos.routes.js
+++ lab12/routes/pulpitos.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
+const path = require('path')
 const router = express.Router();
-const pulpo_sitio = '<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8" /></head><body><h1 style="color: teal">PULPOS BAYBEE</h1></body></html>'
 
 // Ruta general
 router.get("/", (request, response, next) => {
-    response.send(pulpo_sitio);
+    response.render(path.join('pulpo.ejs'));
 });
 
 router.post("/", (request, response, next) => {
